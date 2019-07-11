3fe8287f9749e148f11b15b87f85241a3d0b9f62
Mateo <mateo.webdev@gmail.com>
2019-07-11 17:34:02 +0200

    Ajax API Request

--- rooting-api/src/App.js
+++ rooting-api/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: []
+  }
+  componentDidMount() {
+    this.getTodos()
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        todos: data
+      })
+    })
+  }
+
+  getTodos(){
+    return fetch('http://jsonplaceholder.typicode.com/todos')
+  }
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <header className="App-header">Le routage</header>
+        <Todos/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ rooting-api/src/components/Todos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todos = () => {
+    return(
+        <div>
+            <ul>
+                <li>aze</li>
+                <li>aze</li>
+                <li>aze</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Todos
